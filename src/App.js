11232c410bc77262329b9886df163c698a8e4d3a
jh.lee <jh.lee@exinno.net>
2022-04-13 14:27:26 +0900

    props, event 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import "./App.css";
 import { Component } from "react";
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web
-      </header>
-    );
-  }
-}
-
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+import Subject from "./components/Subject"
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      mode:'welcome',
+      //component이름: {props_name: "props_value"}
+      welcome:{title: 'Welcome', desc:'Hello, React'},
+      subject:{title: 'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc: 'HTML is for information'},
+        {id:2, title:'CSS', desc: 'CSS is for design'},
+        {id:3, title:'JS', desc: 'JS if for interactive'},
+      ]
+    }
+  }
   render() {
+    let _title, _desc = null
+    if (this.state.mode === 'welcome') {
+       _title = this.state.welcome.title
+       _desc = this.state.welcome.desc
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+        onChangePages={function(){
+          this.setState({mode:'read'})
+        }.bind(this)}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
