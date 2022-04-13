75e73e16038101c7e1b6ddce998f6758760df625
jh.lee <jh.lee@exinno.net>
2022-04-13 17:17:20 +0900

    event to change state

---
+++ src/components/Control.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Subject extends Component {
+  render() {
+    return (
+      <ul>
+          <li><a herf="/create" onClick={function(e){
+            e.preventDefault()
+            this.props.onChangeMode('create')
+          }.bind(this)}>create</a></li>
+
+          <li><a herf="/update" onClick={function(e){
+            e.preventDefault()
+            this.props.onChangeMode('update')
+          }.bind(this)}>update</a></li>
+          
+          <li><input type='button' value='delete' onClick={function(e){
+            e.preventDefault()
+            this.props.onChangeMode('delete')
+          }.bind(this)}></input></li>
+        </ul>
+    );
+  }
+}
+
+export default Subject
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 class TOC extends Component {
+  //react는 props, state가 바뀌면 모든 component가 re-render가 되는데, 그것을 막는 식 
+  shouldComponentUpdate(newProps, newState) {
+    if(this.props.data === newProps.data) {
+      return false
+    }
+    return true  
+  }
+  
   render() {
     let lists = []
     let data = this.props.data
     let i = 0
      
     while(i < data.length) {
-      lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      lists.push(<li key={data[i].id}>
+        <a 
+        href={"/content/"+data[i].id}
+        data-id={data[i].id}
+        onClick={function(e){
+        e.preventDefault()
+        this.props.onChangePage(e.target.dataset.id)
+      }.bind(this)}>{data[i].title}</a></li>)
       i = i + 1
     }
     
